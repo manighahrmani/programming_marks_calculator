910b555e15dbc1c176915203854bb009504b37fc
manighahrmani <mashapicasso@gmail.com>
2024-06-05 16:21:51 +0100

    refactor: Make mark calculation and message based on mark into functions not methods of a class

--- lib/backend/marks_calculator.dart
+++ lib/backend/marks_calculator.dart
@@ -1,19 +1,17 @@
 import '../data/assessment_data.dart';
 
-class MarksCalculator {
-  double calculateTotalMarks(Map<String, int> marks) {
-    double total = 0;
-    for (final entry in assessmentWeights.entries) {
-      total += marks[entry.key]! * entry.value;
-    }
-    return total;
+double calculateTotalMarks(Map<String, int> marks) {
+  double total = 0;
+  for (final entry in assessmentWeights.entries) {
+    total += marks[entry.key]! * entry.value;
   }
+  return total;
+}
 
-  String getMessage(double totalMarks) {
-    if (totalMarks >= 40) {
-      return "Congratulations, you have passed in the first attempt!";
-    } else {
-      return "You need to hang around in July and do another assessment.";
-    }
+String getMessage(double totalMarks) {
+  if (totalMarks >= 40) {
+    return "Congratulations, you have passed in the first attempt!";
+  } else {
+    return "You need to hang around in July and do another assessment.";
   }
 }
